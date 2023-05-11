7a28ba8e7184e7c569228f6b99275f82afb4cdfa
Anton <anbelokamensky@gmail.com>
2023-05-11 23:38:39 +0300

    .

--- fireBaseChat/fireBaseChat/Controllers/Login/LoginViewController.swift
+++ fireBaseChat/fireBaseChat/Controllers/Login/LoginViewController.swift
@@ -8,45 +8,73 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    private var imageView: UIImageView {
+    
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(named: "system.message.fill")
-        imageView.tintColor = .link
+        imageView.image = UIImage(named: "logo")
         imageView.contentMode = .scaleAspectFit
         return imageView
-    }
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email Adress..."
+        return field
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .lightGray
+        
+        view.backgroundColor = .link
         title = "Log in"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
-     
         
-        view.addSubview(imageView)
+        
+        // Add subviews
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        let size = view.frame.size.width / 3
-        imageView.frame = CGRect(x: (view.frame.size.width - size) / 2,
-                                 y: 20,
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        imageView.frame = CGRect(x: (scrollView.width - size) / 2,
+                                 y: 30,
                                  width: size,
                                  height: size)
-    }
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+    
+}
 
 
 @objc private func didTapRegister() {
     let vc = RegisterViewController()
     vc.title = "Create account"
     navigationController?.pushViewController(vc, animated: true)
-        }
+}
 
 }
